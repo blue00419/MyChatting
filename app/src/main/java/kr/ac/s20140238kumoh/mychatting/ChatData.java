d6197690151a3d659f269d323cf13f67a64a33d3
DESKTOP-JNIQ4P7\kjh <blue00420@kumoh.ac.kr>
2020-04-23 19:51:25 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/s20140238kumoh/mychatting/ChatData.java
@@ -0,0 +1,22 @@
+package kr.ac.s20140238kumoh.mychatting;
+
+public class ChatData {
+    private String msg;
+    private String nickname;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
